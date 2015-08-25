06f1200f7781581b0bd37a90d669de8894a18668
Giuseppe Lorusso <giuseppelorusso92@gmail.com>
2015-08-25 10:27:27 +0200

    test distance matrix

--- thesis/src/Temp.java
+++ thesis/src/Temp.java
@@ -4,13 +4,6 @@ import distance.EuclideanDissimilarity;
 public class Temp {
 
 	public static void main(String[] args) {
-		Datapoint dp1 = new Datapoint(1, 3, 4, 5);
-		Datapoint dp2 = new Datapoint(2, 3, 4, 5);
-		
-		EuclideanDissimilarity dissimilarity = new EuclideanDissimilarity(dp1, dp2);
-		double result = dissimilarity.compute();
-		
-		System.out.println("dissimilarity: " + result);
 	}
 
 }
--- thesis/src/clustering/DistanceMatrix.java
+++ thesis/src/clustering/DistanceMatrix.java
@@ -1,5 +1,8 @@
 package clustering;
 
+//import java.util.LinkedList;
+//import java.util.List;
+
 import data.Data;
 import data.Datapoint;
 import distance.EuclideanDissimilarity;
@@ -20,18 +23,52 @@ public class DistanceMatrix {
 			
 			for(Object obj2 : data){
 				Datapoint dp2 = (Datapoint) obj2;
-				int x = dp1.getID();
-				int y = dp2.getID();
+				int x = dp1.getID()-1; //perchè gli indici partono da 1
+				int y = dp2.getID()-1;
 				matrix[x][y] = new EuclideanDissimilarity(dp1, dp2).compute();
 			}
 		}
 	}
 	
 	double getDistance(Datapoint dp1, Datapoint dp2){
-		int x = dp1.getID();
-		int y = dp2.getID();
+		int x = dp1.getID() - 1;
+		int y = dp2.getID() - 1;
 		
 		return this.matrix[x][y];
 	}
+	
+	/*--------DA USARE SOLO PER I TEST ----------
+	public DistanceMatrix(List<Datapoint> points) {
+		int n = points.size();
+		this.matrix = new double[n][n];
+		
+		for(int i = 0; i < n; i++){
+			Datapoint dp1 = points.get(i);
+			for(int j = 0; j < n; j++){
+				Datapoint dp2 = points.get(j); 
+				int x = dp1.getID() - 1;
+				int y = dp2.getID() - 1;
+				matrix[x][y] = new EuclideanDissimilarity(dp1, dp2).compute();
+			}
+		}
+	}
+	*/
+	
+	/*Test
+	public static void main(String[] args) {
+		Datapoint p1 = new Datapoint(1, 2, 3, 4);
+		Datapoint p2 = new Datapoint(2, 1, 1, 1);
+		Datapoint p3 = new Datapoint(3, 4, 6, 7);
+		
+		LinkedList<Datapoint> list = new LinkedList<Datapoint>();
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		
+		DistanceMatrix matrix = new DistanceMatrix(list);
+		double dissimilarity = matrix.getDistance(p1, p2);
+		System.out.println("dissimilarity: " + dissimilarity);
+	}
+	*/
 	
 }
